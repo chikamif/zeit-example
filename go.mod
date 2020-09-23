2d33cd34b41d520d0a89c3bffbceade2fcdcf51b
chikami <chikami@fujisan.co.jp>
2020-09-23 12:25:45 +0900

    Fix ical

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/chikamif/zeit-example
 
 go 1.13
+
+require github.com/emersion/go-ical v0.0.0-20200520150327-4bf02c55c8ca
--- index.go
+++ index.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
+
+	"github.com/emersion/go-ical"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
@@ -30,8 +33,32 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "access error:", url)
 		return
 	}
+
 	w.WriteHeader(resp.StatusCode)
 	w.Header().Set("Content-type", "text/calendar; charset=utf-8")
 	w.Header().Set("Content-Disposition", "inline; filename=calendar.ics")
-	io.Copy(w, resp.Body)
+	err = fixical(resp.Body, w)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprint(w, "fix ical error:", err)
+	}
+}
+
+func fixical(r io.Reader, w io.Writer) error {
+	dec := ical.NewDecoder(r)
+	cal, err := dec.Decode()
+	if err != nil {
+		return err
+	}
+
+	newcal := ical.NewCalendar()
+	newcal.Props.SetText(ical.PropVersion, cal.Props.Get(ical.PropVersion).Value)
+	newcal.Props.SetText(ical.PropProductID, cal.Props.Get(ical.PropProductID).Value)
+
+	for _, event := range cal.Events() {
+		event.Props.SetText(ical.PropUID, fmt.Sprint(time.Now().UnixNano()))
+		event.Props.SetDateTime(ical.PropDateTimeStamp, time.Now())
+		newcal.Children = append(newcal.Children, event.Component)
+	}
+	return ical.NewEncoder(w).Encode(newcal)
 }
